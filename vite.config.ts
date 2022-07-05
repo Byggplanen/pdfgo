00a7dee198cf1684518f99df2a01b524051bf248
voidiz <voidqt@hotmail.com>
2022-07-05 16:29:30 +0200

    make actions deploy example instead of library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "",
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: "./src/index.ts",
-      name: "pdfgo",
-      fileName: (format) => `pdfgo.${format}.js`,
+export default defineConfig(({ mode }) => {
+  const sharedConfig = { base: "" };
+
+  if (mode === "example") {
+    // Not in library mode if using `--mode example`.
+    // Serves example instead.
+    return sharedConfig;
+  }
+
+  return {
+    ...sharedConfig,
+    build: {
+      sourcemap: true,
+      lib: {
+        entry: "./src/index.ts",
+        name: "pdfgo",
+        fileName: (format) => `pdfgo.${format}.js`,
+      },
     },
-  },
+  };
 });
