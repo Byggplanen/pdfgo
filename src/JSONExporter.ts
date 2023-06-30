c8f96cf9057b887a213254ae0410c724c537ab82
infernosquad <infzanoza@gmail.com>
2023-06-30 14:22:15 +0400

    Extract JSON capabilities in separate class

---
+++ src/JSONExporter.ts
@@ -0,0 +1,161 @@
+import "@geoman-io/leaflet-geoman-free";
+import L, {MarkerOptions, PathOptions} from "leaflet";
+import {CloudPolylineRenderer, cloudPolylineRenderer} from "./plugins/CloudPolyline/CloudPolylineRenderer";
+import {ArrowRenderer, arrowRenderer} from "./plugins/Measure/ArrowRenderer";
+import Ruler from "./plugins/Measure/Ruler";
+import Area from "./plugins/Measure/Area";
+
+type JSONRenderer = 'CloudPolylineRenderer' | 'ArrowRenderer'
+type JSONType = 'Circle' | 'Polyline' | 'Marker' | 'Polygon' | 'Circlemarker';
+type JSONShapeType = 'Area' | 'ArrowLine';
+
+type JSONShape = {
+  type?: JSONShapeType,
+  tooltip?: string
+}
+
+interface JSONLayer {
+  type: JSONType | null,
+  latLngs?: any
+  options: PathOptions,
+  textMarker?: string
+  renderer: JSONRenderer | null
+  icon?: string
+  shape: JSONShape
+}
+
+interface JSONExporterProps {
+  map: L.Map
+}
+
+export default class JSONExporter {
+
+  private map: L.Map;
+  constructor({ map }: JSONExporterProps) {
+    this.map = map
+  }
+
+  import(json: string): void {
+    JSON.parse(json).forEach((feature: JSONLayer) => {
+      const options: PathOptions = {...feature.options}
+
+      switch (feature.renderer) {
+        case 'CloudPolylineRenderer':
+          options.renderer = cloudPolylineRenderer(feature.options)
+          break;
+        case 'ArrowRenderer':
+          options.renderer = arrowRenderer(feature.options)
+          break;
+      }
+
+      if(feature.latLngs === undefined){
+        return;
+      }
+
+      switch (feature.type) {
+        case "Circle":
+          new L.Circle(feature.latLngs, options).addTo(this.map);
+          break;
+        case "Polyline":
+          const polyline = new L.Polyline(feature.latLngs, options)
+
+          if (feature.shape.tooltip !== undefined) {
+            polyline.setText(feature.shape.tooltip, Ruler.TEXT_OPTIONS)
+          }
+
+          polyline.addTo(this.map);
+          break;
+        case "Marker":
+          const markerOptions = {...feature.options} as MarkerOptions
+          markerOptions.draggable = true
+
+          if (feature.textMarker !== undefined) {
+            markerOptions.textMarker = true;
+            markerOptions.text = feature.textMarker
+            markerOptions.icon = undefined;
+          } else {
+            markerOptions.icon = new L.Icon.Default()
+          }
+
+          const marker = new L.Marker(feature.latLngs, markerOptions)
+          marker.addTo(this.map);
+          break;
+        case "Polygon":
+          const polygon = new L.Polygon(feature.latLngs, options);
+
+          if (feature.shape.tooltip !== undefined) {
+            polygon.bindTooltip(feature.shape.tooltip, Area.TOOLTIP_OPTIONS)
+          }
+
+          polygon.addTo(this.map);
+          break;
+        case "Circlemarker":
+          new L.CircleMarker(feature.latLngs, options).addTo(this.map);
+          break;
+      }
+    })
+  }
+
+  export(): string {
+
+    const json: JSONLayer[] = []
+
+    this.map.eachLayer(layer => {
+      if (!(layer instanceof L.Circle ||
+          layer instanceof L.Marker ||
+          layer instanceof L.CircleMarker ||
+          layer instanceof L.Polygon ||
+          layer instanceof L.Polyline)) {
+        return;
+      }
+
+      const jsonLayer: JSONLayer = {
+        type: null,
+        options: layer.options,
+        renderer: null,
+        shape: {
+          type: undefined,
+          tooltip: undefined
+        }
+      }
+
+      if (layer instanceof L.CircleMarker) {
+        if (layer instanceof L.Circle) {
+          jsonLayer.type = "Circle"
+          jsonLayer.latLngs = layer.getBounds().getCenter()
+        } else {
+          jsonLayer.type = "Circlemarker"
+          jsonLayer.latLngs = layer.getLatLng()
+        }
+      } else if (layer instanceof L.Polyline) {
+        if (layer.options.renderer instanceof CloudPolylineRenderer) {
+          jsonLayer.renderer = 'CloudPolylineRenderer'
+        } else if(layer.options.renderer instanceof ArrowRenderer) {
+          jsonLayer.renderer = 'ArrowRenderer'
+          jsonLayer.shape.tooltip = layer._text
+        }
+        if (layer.getTooltip() !== undefined) {
+          jsonLayer.shape.tooltip = layer.getTooltip()?.getContent()?.toString()
+        }
+        jsonLayer.latLngs = layer.getLatLngs()
+        jsonLayer.options.renderer = undefined
+        if (layer instanceof L.Polygon) {
+          jsonLayer.type = "Polygon"
+        } else {
+          jsonLayer.type = "Polyline"
+        }
+      } else if (layer instanceof L.Marker) {
+        jsonLayer.type = "Marker"
+        jsonLayer.latLngs = layer.getLatLng()
+        if (layer.options.icon instanceof L.DivIcon) {
+          const html = layer.options.icon.options.html as HTMLTextAreaElement
+          jsonLayer.textMarker =  html.value
+        }
+      }
+
+      json.push(jsonLayer);
+    })
+
+    return JSON.stringify(json);
+  }
+}
--- src/PDFGo.ts
+++ src/PDFGo.ts
@@ -1,4 +1,4 @@
-import L, {MarkerOptions, PathOptions} from "leaflet";
+import L from "leaflet";
 import "@geoman-io/leaflet-geoman-free";
 
 import "./patches/fix-leaflet-marker";
@@ -14,10 +14,7 @@ import { ColorClickCallback, colorPicker } from "./plugins/ColorPicker";
 
 import "./PDFGo.css";
 import { save } from "./plugins/Save";
-import {CloudPolylineRenderer, cloudPolylineRenderer} from "./plugins/CloudPolyline/CloudPolylineRenderer";
-import {arrowRenderer, ArrowRenderer} from "./plugins/Measure/ArrowRenderer";
-import Ruler from "./plugins/Measure/Ruler";
-import Area from "./plugins/Measure/Area";
+import JSONExporter from "./JSONExporter";
 
 type SaveClickCallback = (bytes?: Uint8Array) => Promise<void>;
 type ChangeCallback = () => void
@@ -71,25 +68,6 @@ type PDFGoProps = {
   saveSettings?: SaveSettings;
 };
 
-type JSONRenderer = 'CloudPolylineRenderer' | 'ArrowRenderer'
-type JSONType = 'Circle' | 'Polyline' | 'Marker' | 'Polygon' | 'Circlemarker';
-type JSONShapeType = 'Area' | 'ArrowLine';
-
-type JSONShape = {
-  type?: JSONShapeType,
-  tooltip?: string
-}
-
-interface JSONLayer {
-  type: JSONType | null,
-  latLngs?: any
-  options: PathOptions,
-  textMarker?: string
-  renderer: JSONRenderer | null
-  icon?: string
-  shape: JSONShape
-}
-
 export default class PDFGo {
   private map: L.Map;
 
@@ -125,6 +103,8 @@ export default class PDFGo {
 
   private json: string = ''
 
+  private jsonExporter: JSONExporter
+
   constructor({
     element,
     pageNumber = 1,
@@ -153,6 +133,7 @@ export default class PDFGo {
     this.initializeHandlers();
     this.initializeToolbar();
     this.onChange();
+    this.jsonExporter = new JSONExporter({map: this.map})
   }
 
   getColor(): string {
@@ -176,128 +157,12 @@ export default class PDFGo {
   }
 
   importFromJSON(json: string): void {
-    JSON.parse(json).forEach((feature: JSONLayer) => {
-      const options: PathOptions = {...feature.options}
-
-      switch (feature.renderer) {
-        case 'CloudPolylineRenderer':
-          options.renderer = cloudPolylineRenderer(feature.options)
-          break;
-        case 'ArrowRenderer':
-          options.renderer = arrowRenderer(feature.options)
-          break;
-      }
-
-      if(feature.latLngs === undefined){
-        return;
-      }
-
-        switch (feature.type) {
-          case "Circle":
-            new L.Circle(feature.latLngs, options).addTo(this.map);
-            break;
-          case "Polyline":
-            const polyline = new L.Polyline(feature.latLngs, options)
-
-            if (feature.shape.tooltip !== undefined) {
-              polyline.setText(feature.shape.tooltip, Ruler.TEXT_OPTIONS)
-            }
-
-            polyline.addTo(this.map);
-            break;
-          case "Marker":
-            const markerOptions = {...feature.options} as MarkerOptions
-            markerOptions.draggable = true
-
-            if (feature.textMarker !== undefined) {
-              markerOptions.textMarker = true;
-              markerOptions.text = feature.textMarker
-              markerOptions.icon = undefined;
-            } else {
-              markerOptions.icon = new L.Icon.Default()
-            }
-
-            const marker = new L.Marker(feature.latLngs, markerOptions)
-            marker.addTo(this.map);
-            break;
-          case "Polygon":
-            const polygon = new L.Polygon(feature.latLngs, options);
-
-            if (feature.shape.tooltip !== undefined) {
-              polygon.bindTooltip(feature.shape.tooltip, Area.TOOLTIP_OPTIONS)
-            }
-
-            polygon.addTo(this.map);
-            break;
-          case "Circlemarker":
-            new L.CircleMarker(feature.latLngs, options).addTo(this.map);
-            break;
-        }
-      })
+    this.jsonExporter.import(json)
     this.changed = false
   }
 
   getJSON(): string {
-
-    const json: JSONLayer[] = []
-
-    this.map.eachLayer(layer => {
-      if (!(layer instanceof L.Circle ||
-          layer instanceof L.Marker ||
-          layer instanceof L.CircleMarker ||
-          layer instanceof L.Polygon ||
-          layer instanceof L.Polyline)) {
-        return;
-      }
-
-      const jsonLayer: JSONLayer = {
-        type: null,
-        options: layer.options,
-        renderer: null,
-        shape: {
-          type: undefined,
-          tooltip: undefined
-        }
-      }
-
-      if (layer instanceof L.CircleMarker) {
-        if (layer instanceof L.Circle) {
-          jsonLayer.type = "Circle"
-          jsonLayer.latLngs = layer.getBounds().getCenter()
-        } else {
-          jsonLayer.type = "Circlemarker"
-          jsonLayer.latLngs = layer.getLatLng()
-        }
-      } else if (layer instanceof L.Polyline) {
-        if (layer.options.renderer instanceof CloudPolylineRenderer) {
-          jsonLayer.renderer = 'CloudPolylineRenderer'
-        } else if(layer.options.renderer instanceof ArrowRenderer) {
-          jsonLayer.renderer = 'ArrowRenderer'
-          jsonLayer.shape.tooltip = layer._text
-        }
-        if (layer.getTooltip() !== undefined) {
-          jsonLayer.shape.tooltip = layer.getTooltip()?.getContent()?.toString()
-        }
-        jsonLayer.latLngs = layer.getLatLngs()
-        jsonLayer.options.renderer = undefined
-        if (layer instanceof L.Polygon) {
-          jsonLayer.type = "Polygon"
-        } else {
-          jsonLayer.type = "Polyline"
-        }
-      } else if (layer instanceof L.Marker) {
-        jsonLayer.type = "Marker"
-        jsonLayer.latLngs = layer.getLatLng()
-        if (layer.options.icon instanceof L.DivIcon) {
-          const html = layer.options.icon.options.html as HTMLTextAreaElement
-          jsonLayer.textMarker =  html.value
-        }
-      }
-
-      json.push(jsonLayer);
-    })
-
-    this.json = JSON.stringify(json)
+    this.json = this.jsonExporter.export()
 
     return this.json
   }
